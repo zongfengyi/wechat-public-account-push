a092adfcb69eb63d86eb7d70e3101eb8191ba1a4
zongfengyi <119508987+zongfengyi@users.noreply.github.com>
2022-11-30 23:44:52 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx7cfe058a83215120',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'a8a04a3d40a061a95a1c092c450cd40a',
 
   PROVINCE: '江苏',
   CITY: '无锡',
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'o9CYF6ftI8uZeBwadXAvVFDZUqrg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-22',
       festivals: [
@@ -40,9 +40,8 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2020-11-24' },
+     
       ],
     },
   ],
