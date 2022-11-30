59ac952e18d9f0d30e6179f45106005b854d697b
zongfengyi <119508987+zongfengyi@users.noreply.github.com>
2022-11-30 23:57:53 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o9CYF6b4nhxzXaMAj8ROrOZz9siA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'o9CYF6ftI8uZeBwadXAvVFDZUqrg',
+      useTemplateId: 'wx7cfe058a83215120',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-22',
       festivals: [
