7b01b3f31696cea4b7d5a4516f6b3ca89ec1bcc0
zongfengyi <119508987+zongfengyi@users.noreply.github.com>
2022-11-30 23:50:55 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o9CYF6b4nhxzXaMAj8ROrOZz9siA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'o9CYF6ftI8uZeBwadXAvVFDZUqrg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -54,11 +54,11 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o9CYF6b4nhxzXaMAj8ROrOZz9siA',
     }
   ],
 
-}
+
 
 module.exports = USER_CONFIG
 
